08e6bdbb177bbf7f3b1a693e67a2c8b8559f6676
ziaoang <ziaoang@gmail.com>
2017-06-12 19:04:56 +0800

    add error handler

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -1,3 +1,3 @@
 var g_config = {};
-// g_config.api_url = "http://127.0.0.1:5000/";
-g_config.api_url = "http://59.108.48.35:8080/";
+g_config.api_url = "http://127.0.0.1:5000";
+// g_config.api_url = "http://59.108.48.35:8080";
